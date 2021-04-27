2f2981744d398de3971f94cd8b977eceb492ed73
camillaskar <camonaskar@gmail.com>
2021-04-27 22:17:04 +0800

    logo icon

--- app/src/main/java/com/example/pineapple/MainActivity.kt
+++ app/src/main/java/com/example/pineapple/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity(), Navigation {
     }
 
 
+
 //    fun openFragment(fragment: Fragment){
 //        supportFragmentManager
 //                .beginTransaction()
